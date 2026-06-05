4df23a0a3aa2a35a033b9bf0f5a1eef8a8086b03
ODespertardoAmor <escritorairanelmalemos@gmail.com>
2026-06-04 23:40:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -393,15 +393,15 @@ with app.app_context():
         print("Os campos provavelmente já existem ou ocorreu um erro:", e)
 
 # CÓDIGO TEMPORÁRIO - REMOVA DEPOIS DE SUBIR UMA VEZ
-with app.app_context():
-    try:
-        db.session.execute(db.text("ALTER TABLE mensagens ADD COLUMN lida BOOLEAN DEFAULT FALSE;"))
-        db.session.execute(db.text("ALTER TABLE mensagens ADD COLUMN visualizacao_unica BOOLEAN DEFAULT FALSE;"))
-        db.session.execute(db.text("ALTER TABLE mensagens ADD COLUMN audio VARCHAR(300);"))
-        db.session.commit()
-        print("Campos adicionados com sucesso no Postgres do Render!")
-    except Exception as e:
-        print("Os campos provavelmente já existem ou ocorreu um erro:", e)
+#with app.app_context():
+    #try:
+       # db.session.execute(db.text("ALTER TABLE mensagens ADD COLUMN lida BOOLEAN DEFAULT FALSE;"))
+        #db.session.execute(db.text("ALTER TABLE mensagens ADD COLUMN visualizacao_unica BOOLEAN DEFAULT FALSE;"))
+        #db.session.execute(db.text("ALTER TABLE mensagens ADD COLUMN audio VARCHAR(300);"))
+        #db.session.commit()
+       # print("Campos adicionados com sucesso no Postgres do Render!")
+    #except Exception as e:
+        #print("Os campos provavelmente já existem ou ocorreu um erro:", e)
 # ====================================
 # INICIALIZAÇÃO
 # ====================================
